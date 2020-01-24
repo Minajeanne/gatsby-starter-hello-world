3848ead28446477fefd2693e9efb3a9e3fd61924
Minajeanne <albrown07@mac.com>
2020-01-24 15:57:17 -0500

    add props

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,8 @@
 import React from "react"
-import Header from '../components/header.js';
 
 export default () => (
   <div style={{ color: `purple` }}>
-    <Header />
+    <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>

c8c3656c355b8e00de140541b458c4360324d0e7
Minajeanne <albrown07@mac.com>
2020-01-24 15:46:32 -0500

    random img

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ export default () => (
   <div style={{ color: `purple` }}>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
-    <img src="https://unsplash.com/photos/jrS7tPQH1q0" alt="" />
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
 )

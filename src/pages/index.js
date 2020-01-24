a0801ec7e37d076a2f092de545b0a1475e0dd2c2
Minajeanne <albrown07@mac.com>
2020-01-24 15:45:15 -0500

    add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,5 +4,6 @@ export default () => (
   <div style={{ color: `purple` }}>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
+    <img src="https://unsplash.com/photos/jrS7tPQH1q0" alt="" />
   </div>
 )
